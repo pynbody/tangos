baa4a976cd7e5d822c398aa63b7c9eb1be0842e5
Andrew Pontzen <a.pontzen@ucl.ac.uk>
2015-08-22 14:48:08 -0700

    Add environment setup

--- environment.sh
+++ environment.sh
@@ -1,3 +1,5 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 export PYTHONPATH=$DIR/modules/:$PYTHONPATH
 export PATH=$DIR/tools/:$PATH
+export HALODB_ROOT=$HOME/Science/db_galaxies/
+export HALODB_DEFAULT_DB=$HOME/Science/halodb/data.db
