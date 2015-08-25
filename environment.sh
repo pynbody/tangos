493c9e360ccfff9dca60969596eacbf8473370a0
Andrew Pontzen <a.pontzen@ucl.ac.uk>
2015-08-25 10:22:36 -0700

    Do not set HALODB_DEFAULT_DB if it is already set

--- environment.sh
+++ environment.sh
@@ -2,7 +2,10 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 export PYTHONPATH=$DIR/modules/:$PYTHONPATH
 export PATH=$DIR/tools/:$PATH
 export HALODB_ROOT=$DIR/../db_galaxies/
-export HALODB_DEFAULT_DB=$DIR/data.db
+
+if [ -z "$HALODB_DEFAULT_DB" ]; then
+    export HALODB_DEFAULT_DB=$DIR/data.db
+fi
 
 if [[ -e enivornment_local.sh ]]
 then
