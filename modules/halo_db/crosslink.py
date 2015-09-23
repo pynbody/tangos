98a45f31a0427812a7bb9a4ba2544dac71c36c5e
Andrew Pontzen <a.pontzen@ucl.ac.uk>
2015-09-23 09:30:22 +0100

    Unify tracking/crossmatching code

---
+++ modules/halo_db/crosslink.py
@@ -0,0 +1,52 @@
+from db_crosslink import session
+import halo_db as db
+import traceback
+
+def get_halo_entry(ts, halo_number):
+    h = ts.halos.filter_by(halo_number=halo_number).first()
+    return h
+
+
+def need_crosslink_ts(ts1,ts2):
+    same_d_id = db.get_or_create_dictionary_item(session, "ptcls_in_common").id
+    sources = [h.id for h in ts1.halos.all()]
+    targets = [h.id for h in ts2.halos.all()]
+    if len(targets)==0 or len(sources)==0:
+        print "--> no halos"
+        return False
+
+    exists = session.query(db.HaloLink).filter(db.and_(db.HaloLink.halo_from_id.in_(sources), db.HaloLink.relation_id == same_d_id, db.HaloLink.halo_to_id.in_(targets))).count() > 0
+    if exists:
+        print "--> existing objects"
+    return not exists
+
+
+def create_db_objects_from_catalog(cat, ts1, ts2):
+    same_d_id = db.get_or_create_dictionary_item(session, "ptcls_in_common")
+
+    for i, possibilities in enumerate(cat):
+        h1 = get_halo_entry(ts1,i)
+        for cat_i, weight in possibilities:
+            h2 = get_halo_entry(ts2,cat_i)
+
+            if h1 is not None and h2 is not None:
+                print i,cat_i,h1,h2,weight
+                cx = db.HaloLink(h1, h2, same_d_id, weight)
+                session.merge(cx)
+    session.commit()
+
+
+def crosslink_ts(ts1, ts2, halo_min=0, halo_max=100):
+    snap1 = ts1.load()
+    snap2 = ts2.load()
+
+    try:
+        cat = snap1.bridge(snap2).fuzzy_match_catalog(halo_min, halo_max, threshold=0.05)
+        back_cat = snap2.bridge(snap1).fuzzy_match_catalog(halo_min,halo_max, threshold=0.05)
+    except:
+        print "ERROR"
+        traceback.print_exc(file=sys.stderr)
+        return
+
+    create_db_objects_from_catalog(cat, ts1, ts2)
+    create_db_objects_from_catalog(back_cat, ts2, ts1)
--- tools/db_timelink.py
+++ tools/db_timelink.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python2.7
 
-import sqlalchemy
-import time
-import pynbody
-import sys
+
 import halo_db as db
 import argparse
-from halo_db import parallel_tasks
-from terminalcontroller import term
+from halo_db import parallel_tasks, crosslink
+
 
 db.use_blocking_session()
 session = db.core.internal_session
@@ -68,99 +65,9 @@ else:
 
 parallel_tasks.mpi_sync_db(session)
 
-time_dictionary = db.get_or_create_dictionary_item(session, "time")
-
 session.commit()
 
 for s_x, s in pair_list:
-
-    print "Start process: ", s.filename[-6:], s_x.filename[-6:]
-
-    targets = [x.id for x in session.query(
-        db.Halo).filter_by(timestep=s_x).all()]
-    # truncate targets list otherwise SQL can't cope. This is only for
-    # checking whether there is SOMETHING there so should be fine.
-    targets = targets[:100]
-
-    if (not force) and session.query(db.HaloLink).filter(db.and_(db.HaloLink.halo_from_id.in_(targets), db.HaloLink.relation_id == time_dictionary.id)).count() > 0:
-        print term.GREEN + " Existing link objects -- moving on! (Use force on command line to override)" + term.NORMAL
-        continue
-
-    f = pynbody.load(s.filename, only_header=True)
-
-    f_x = pynbody.load(s_x.filename, only_header=True)
-
-    try:
-        f.halos()
-        f_x.halos()
-    except:
-        print "Failure loading halo cat - continuing"
-        continue
-
-    try:
-        min = 1
-        max = len(f.halos())
-        max2 = len(f_x.halos())
-        max = max if max < max2 else max2
-        max = max if max < max_gp else max_gp
-        print "cat=", min, max
-        if max < 2:
-            continue
-
-        if "no_iord" in sys.argv or len(f.gas) == 0 and len(f_x.gas) == 0:
-            print "Briding WITHOUT iords"
-            cat = pynbody.bridge.Bridge(f_x, f).match_catalog(min, max)
-        else:
-            cat = pynbody.bridge.OrderBridge(
-                f_x, f).match_catalog(min, max)
-
-        # cat = match_catalogue(g_x, g, siman.TipsyBridge(f_x, f), 1,40)
-
-        for retries in xrange(10, 0, -1):
-            try:
-
-                # Delete existing entries before inserting new onews
-                # direct query delete doesn't work for some reason,
-                # probably something to do with in_ statement?
-                dl_tab = session.query(db.HaloLink).filter(
-                    db.HaloLink.halo_from_id.in_(targets)).all()
-
-                if len(dl_tab) > 0:
-                    print "Warning: deleting ", dl_tab, " entries"
-                for dl in dl_tab:
-                    session.delete(dl)
-
-                for i in xrange(1, len(cat)):
-                    print "...", i, "->", cat[i]
-                    # record matching from g_x -> g
-                    if cat[i] != -1:
-                        a = session.query(db.Halo).filter(
-                            db.and_(db.Halo.timestep == s_x, db.Halo.halo_number == i)).first()
-                        b = session.query(db.Halo).filter(
-                            db.and_(db.Halo.timestep == s, db.Halo.halo_number == int(cat[i]))).first()
-                        if a != None and b != None:
-                            cx = db.HaloLink(a, b, time_dictionary)
-                            print "Create: ", cx
-                            session.merge(cx)
-                        else:
-                            print "Missing halo database entry for ", s_x, i, s, i - 1
-                    else:
-                        print "No link for: ", session.query(db.Halo).filter(db.and_(db.Halo.timestep == s_x, db.Halo.halo_number == i)).first()
-                session.commit()
-                break
-
-            except sqlalchemy.exc.OperationalError:
-                if retries == 0:
-                    raise
-                session.rollback()
-                print "DB is locked, retrying in 1 second (%d attempts remain)", retries
-                time.sleep(1)
-            else:
-                raise RuntimeError, "Could not write to DB"
-
-    except:
-        session.rollback()
-        raise
-
-    f_x = f
-    s_x = s
+    print s_x,"-->",s
+    if force or crosslink.need_crosslink_ts(s_x, s):
+        crosslink.crosslink_ts(s_x, s, 0, max_gp)
