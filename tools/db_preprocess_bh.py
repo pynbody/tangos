fe9f837d9f57dbfc332142fdc4adff638a318c1d
Andrew Pontzen <a.pontzen@ucl.ac.uk>
2015-11-01 11:02:26 -0800

    Restore all steps to db_preprocess_bh.py

--- tools/db_preprocess_bh.py
+++ tools/db_preprocess_bh.py
@@ -3,6 +3,6 @@
 import Simpy.BlackHoles.orbit, Simpy.BlackHoles.mergers
 import sys
 
-#Simpy.BlackHoles.orbit.sepOrbitbyStep(sys.argv[1], minstep = 1, maxstep = 4096)
-#Simpy.BlackHoles.orbit.truncOrbitFile(sys.argv[1], minstep = 1, maxstep = 4096) 
+Simpy.BlackHoles.orbit.sepOrbitbyStep(sys.argv[1], minstep = 1, maxstep = 4096)
+Simpy.BlackHoles.orbit.truncOrbitFile(sys.argv[1], minstep = 1, maxstep = 4096) 
 Simpy.BlackHoles.mergers.reducedata(sys.argv[1],outname='changa.out*',mergename='BHmerge.txt',NCHILADA=False)
