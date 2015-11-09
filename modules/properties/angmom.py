b41764ebd1007d4ec5e9375128a9699c2b22cf0f
mtremmel <m.tremmel6@gmail.com>
2015-11-08 16:01:03 -0800

    add angmom properties (for HI gas only right now). calculates total angular momentum of HI gas as well as an angular momentum profile for HI gas.

---
+++ modules/properties/angmom.py
@@ -0,0 +1,68 @@
+from . import HaloProperties
+import numpy as np
+import math
+import pynbody
+import time
+import copy
+
+@pynbody.analysis.profile.Profile.profile_property
+def j_HI(self):
+    """
+    Magnitude of the total angular momentum in HI as a function of distance from halo center
+    """
+    j_HI = np.zeros(self.nbins)
+
+    for i in range(self.nbins):
+        subs = self.sim[self.binind[i]]
+        jx = (subs['j'][:, 0] * subs['mass'] * subs['HI']).sum() / (self['mass'][i] * self['HI'][i])
+        jy = (subs['j'][:, 1] * subs['mass'] * subs['HI']).sum() / (self['mass'][i] * self['HI'][i])
+        jz = (subs['j'][:, 2] * subs['mass'] * subs['HI']).sum() / (self['mass'][i] * self['HI'][i])
+
+        j_HI[i] = np.sqrt(jx ** 2 + jy ** 2 + jz ** 2)
+
+    return j_HI
+
+
+class AngMomHI(HaloProperties):
+
+    def name(self):
+        return "j_HI", "J_HI_profile"
+
+    def requires_property(self):
+        return ['SSC', 'Rvir']
+
+    def rstat(self, halo, maxrad, cen, delta=0.1):
+
+        nbins = int(maxrad / delta)
+        maxrad = delta * (nbins + 1)
+
+        pro = pynbody.analysis.profile.Profile(halo.g, type='lin', ndim=3,
+                                               min=0, max=maxrad, nbins=nbins)
+
+        j_HI_a = pro['j_HI']
+        j_HI_a = np.array(j_HI_a)
+
+        return j_HI_a
+
+    def calculate(self,  halo, properties):
+        if len(halo.g > 100):
+            com = properties['SSC']
+            rad = properties['Rvir']
+            halo["pos"] -= com
+            halo.wrap()
+
+            delta = existing_properties.get('delta',0.1)
+            j_HI_a = rstat(self, halo, rad, com, delta)
+
+            HImass = halo.g['mass']*halo.g['HI']
+            mvec = pynbody.array.SimArray(np.transpose(np.vstack((HImass,HImass,HImass))),'Msol')
+            jvec = np.sum(halo.g['j'] * mvec, axis=0)/np.sum(HImass)
+            j_HI_tot = np.sqrt(np.sum(jvec**2))
+
+            halo["pos"] += com
+            halo.wrap()
+
+            return j_HI_tot, j_HI_a
+
+
+
