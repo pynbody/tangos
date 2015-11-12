fix units issue in BH offset calculation

modules/properties/BH.py
@@ -145,7 +145,7 @@ class BH(HaloProperties):
             final[t] = float(vars[t][entry])
 
         offset = np.array((final['x'],final['y'],final['z']))-main_halo_ssc
-        bad, = np.where(np.abs(offset) > boxsize/2.)
+        bad, = np.where(np.abs(offset.in_units('kpc')) > boxsize.in_units('kpc')/2.)
         offset[bad] = -1.0 * (offset[bad]/np.abs(offset[bad])) * (boxsize - np.abs(offset[bad]))
 
         return final['mdot'], final['mdotmean'], final['mdotsig'], offset, np.linalg.norm(offset), final['mass']