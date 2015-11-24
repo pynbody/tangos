2cc48a770854ed9900629cd8fedd9860b348c9c1
Andrew Pontzen <a.pontzen@ucl.ac.uk>
2015-11-24 22:28:18 +0000

    Add mpi4py backend

---
+++ modules/halo_db/parallel_tasks/backend_mpi4py.py
@@ -0,0 +1,39 @@
+from mpi4py import MPI
+import warnings
+
+comm = MPI.COMM_WORLD
+
+def send(data, destination, tag=0):
+    comm.send(data, dest=destination, tag = tag)
+
+def receive_any(source=None):
+    status = MPI.Status()
+    if source is None:
+        source = MPI.ANY_SOURCE
+    data = comm.recv(source=source, tag=MPI.ANY_TAG, status=status)
+    return data, status.source, status.tag
+
+def receive(source=None, tag=0):
+    if source is None:
+        source = MPI.ANY_SOURCE
+    return comm.recv(source=source,tag=tag)
+
+
+def rank():
+    return comm.Get_rank()
+
+def size():
+    return comm.Get_size()
+
+def barrier():
+    comm.Barrier()
+
+def finalize():
+    MPI.Finalize()
+
+
+def launch(function, num_procs, args):
+    if num_procs is not None:
+        if rank()==0:
+            warnings.warn("Number of processors requested (%d) will be ignored as this is an MPI run that has already selected %d processors"%(num_procs,size()))
+    function(*args)
