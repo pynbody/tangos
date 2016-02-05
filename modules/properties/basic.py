8ef4c46dd4bbdb7eb789feea4d33f139bb739039
mtremmel <m.tremmel6@gmail.com>
2016-02-05 09:20:38 -0800

    change calculation of binned magnitudes... attempt to match better with total halo magnitudes.

--- modules/properties/basic.py
+++ modules/properties/basic.py
@@ -252,8 +252,9 @@ def magnitudes_encl(self,band='v'):
     mag = np.zeros(self.nbins)
     ltot = 0.0
     for i in range(self.nbins):
-        if len(self.sim[self.binind[i]]) > 0:
-            ltot += 10**(-0.4*self['magnitudes,'+band][i])
+        if len(self.binind[i]) > 0:
+            subs = self.sim[self.binind[i]]
+            ltot += np.sum(10**(-0.4*subs[band+'_mag']))
         mag[i] = -2.5 * np.log10(ltot)
 
     return mag
@@ -281,7 +282,7 @@ class ABMagnitudes_encl(HaloProperties):
 
         ABcorr = {'u':0.79,'b':-0.09,'v':0.02,'r':0.21,'i':0.45,'j':0.91,'h':1.39,'k':1.85}
 
-        return V_encl+Bcorr['v'], B_encl+ABcorr['b'], K_encl+ABcorr['k'], U_encl+ABcorr['u'], J_encl+ABcorr['j'], I_encl+ABcorr['i']
+        return V_encl+ABcorr['v'], B_encl+ABcorr['b'], K_encl+ABcorr['k'], U_encl+ABcorr['u'], J_encl+ABcorr['j'], I_encl+ABcorr['i']
 
 class HalfLight(HaloProperties):
     def name(self):
