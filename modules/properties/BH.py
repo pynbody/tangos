f7dd983bc4db018baf950c8d9257e350cab5d81b
mtremmel <m.tremmel6@gmail.com>
2016-02-20 15:16:33 -0800

    debug print

--- modules/properties/BH.py
+++ modules/properties/BH.py
@@ -283,6 +283,7 @@ class BHHosts(HaloProperties):
         return False
 
     def calculate(self, halo, properties):
+        print "here!"
         if properties.halo_type != 1:
             return None
         if properties.host_halo is None:
