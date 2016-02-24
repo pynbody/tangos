d929b76252aa3741872bf2f750eae08ef4a7960c
mtremmel <m.tremmel6@gmail.com>
2016-02-24 08:25:31 -0800

    fix issue where angular momentum is not calculated in some places for HI

--- modules/properties/dynamics.py
+++ modules/properties/dynamics.py
@@ -451,7 +451,6 @@ def j_HI_enc(self):
     for i in range(self.nbins):
         subs = self.sim[self.binind[i]]
         if len(subs)>0:
-            continue
             MHIenc += (subs['mass'] * subs['HI']).sum()
             jpx += (subs['j'][:, 0] * subs['mass'] * subs['HI']).sum()
             jpy += (subs['j'][:, 1] * subs['mass'] * subs['HI']).sum()
