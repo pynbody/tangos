8bebebac0ce6b7e668e766bc9b1493d82700c89d
mtremmel <m.tremmel6@gmail.com>
2016-09-09 15:31:03 -0700

    fix bug in wrapping BH relative distances.

--- modules/properties/BH.py
+++ modules/properties/BH.py
@@ -168,7 +168,7 @@ class BH(HaloProperties):
         else:
             offset = np.array((final['x'],final['y'],final['z']))-main_halo_ssc
             bad, = np.where(np.abs(offset) > boxsize/2.)
-            offset[bad] = -1.0 * (offset[bad]/np.abs(offset[bad])) * (boxsize - np.abs(offset[bad]))
+            offset[bad] = -1.0 * (offset[bad]/np.abs(offset[bad])) * np.abs(boxsize - np.abs(offset[bad]))
 
         return final['mdot'], final['mdotmean'], final['mdotsig'], offset, np.linalg.norm(offset), final['mass']
 
