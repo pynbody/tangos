e53e317100034d4627c4c0fecd67814ec2b48d70
mtremmel <m.tremmel6@gmail.com>
2016-09-23 15:17:35 -0700

    debug print statement

--- modules/tangos/relation_finding/one_hop.py
+++ modules/tangos/relation_finding/one_hop.py
@@ -81,7 +81,7 @@ class HopStrategy(object):
         results = filter(lambda x: x is not None, results)
 
         self._all = results
-        print self._query_ordered.all()
+        print self._query_ordered
 
     def _get_query_all(self):
         if self._all is None:
