f8d570fc74ed925a8c4edc05ae6e87c5f1c43ca0
Andrew Pontzen <a.pontzen@ucl.ac.uk>
2017-08-14 20:06:00 +0100

    Incorporate improved efficiency of finding previous/later timesteps into live calculation

--- modules/tangos/live_calculation/builtin_functions/__init__.py
+++ modules/tangos/live_calculation/builtin_functions/__init__.py
@@ -5,32 +5,43 @@ from tangos.util import consistent_collection
 from .. import BuiltinFunction, FixedInput, FixedNumericInput
 from ... import core
 
-
 @BuiltinFunction.register
 def match(source_halos, target):
-    from ... import relation_finding
-    if not isinstance(target, core.Base):
-        target = tangos.get_item(target)
-    results = relation_finding.MultiSourceMultiHopStrategy(source_halos, target).all()
+    if target is None:
+        results = [None]*len(source_halos)
+    else:
+        from ... import relation_finding
+        if not isinstance(target, core.Base):
+            target = tangos.get_item(target)
+        results = relation_finding.MultiSourceMultiHopStrategy(source_halos, target).all()
     assert len(results) == len(source_halos)
     return np.array(results, dtype=object)
 match.set_input_options(0, provide_proxy=True, assert_class = FixedInput)
 
 @BuiltinFunction.register
 def later(source_halos, num_steps):
-    timestep = consistent_collection.ConsistentCollection(source_halos).timestep
-    for i in xrange(num_steps):
-        timestep = timestep.next
+    timestep = consistent_collection.ConsistentCollection(source_halos).timestep.get_next(num_steps)
     return match(source_halos, timestep)
+
 later.set_input_options(0, provide_proxy=True, assert_class = FixedNumericInput)
+
 
 @BuiltinFunction.register
 def earlier(source_halos, num_steps):
-    timestep = consistent_collection.ConsistentCollection(source_halos).timestep
-    for i in xrange(num_steps):
-        timestep = timestep.previous
-    return match(source_halos, timestep)
+    return later(source_halos, -num_steps)
+
 earlier.set_input_options(0, provide_proxy=True, assert_class = FixedNumericInput)
+
+
+@BuiltinFunction.register
+def latest(source_halos):
+    timestep = consistent_collection.ConsistentCollection(source_halos).timestep.get_final()
+    return match(source_halos, timestep)
+
+@BuiltinFunction.register
+def earliest(source_halos):
+    timestep = consistent_collection.ConsistentCollection(source_halos).timestep.get_final(-1)
+    return match(source_halos, timestep)
 
 
 
