dc594f47af11dc7a301da02fb3d96b07fe46e9a4
Andrew Pontzen <a.pontzen@ucl.ac.uk>
2017-08-31 13:15:36 +0200

    Move to proper egg-based installation (including scripts)

--- modules/tangos/relation_finding/__init__.py
+++
@@ -1,9 +0,0 @@
-from .one_hop import HopStrategy, HopMajorDescendantStrategy, HopMajorProgenitorStrategy
-from .multi_hop import MultiHopStrategy
-from .multi_source import MultiSourceMultiHopStrategy
-from .multi_hop_variants import MultiHopMajorDescendantsStrategy, MultiHopAllProgenitorsStrategy, \
-    MultiHopMajorProgenitorsStrategy, MultiHopMostRecentMergerStrategy
-
-
-
-
