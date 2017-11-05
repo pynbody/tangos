7590a089d6d4131f0a33cfa1c1ddced9b4ec6b6f
mtremmel <m.tremmel6@gmail.com>
2017-11-05 12:26:21 -0500

    properties for Temperature (ew and mw) profiles

---
+++ michaels_properties/profiles.py
@@ -0,0 +1,90 @@
+from tangos.properties import HaloProperties, TimeChunkedProperty
+import numpy as np
+import pynbody
+
+
+def emissivity(rho, T, mu, tcool):
+	kb = pynbody.array.SimArray(1.380658e-16, 'erg K**-1')
+	mh = pynbody.array.SimArray(1.6726219e-24, 'g')
+	return 3. / 2. * rho * kb * T / (mu * mh * tcool)
+
+
+def tcool(rho, T, mu):
+	# taken from https://arxiv.org/abs/astro-ph/9809159 eq 12
+	fm = 1.0  # metalicity dependent factor, 1.0 for solar, 0.03 for pristine
+	mh = pynbody.array.SimArray(1.6726219e-24, 'g')
+	C1 = 3.88e11
+	C2 = 5e7
+	return C1 * mu * mh * T ** 0.5 / (rho * (1 + C2 * fm / T))
+
+
+@pynbody.analysis.profile.Profile.profile_property
+def Tew(self):
+	temp_cut = 1.26e6
+	kb = pynbody.array.SimArray(1.380658e-16, 'erg K**-1')
+	temp = np.zeros(self.nbins)
+	rho_c = pynbody.analysis.cosmology.rho_crit(self.sim, z=0)
+	for i in range(self.nbins):
+		subs = self.sim[self.binind[i]]
+		use = np.where((subs.g['temp'] > temp_cut))[0]
+		mu = 0.58
+		tc = tcool(subs.g['rho'][use].in_units('g cm**-3'), subs.g['temp'][use], mu)
+		em = emissivity(subs.g['rho'][use].in_units('g cm**-3'), subs.g['temp'][use], mu, tc)
+		temp[i] = np.sum(em * subs.g['temp'][use]) / np.sum(em)
+
+	return kb.in_units('keV K**-1') * temp
+
+
+@pynbody.analysis.profile.Profile.profile_property
+def Tmw(self):
+	temp_cut = 1.26e6
+	kb = pynbody.array.SimArray(1.380658e-16, 'erg K**-1')
+	temp = np.zeros(self.nbins)
+	for i in range(self.nbins):
+		subs = self.sim[self.binind[i]]
+		use = np.where((subs.g['temp'] > temp_cut))[0]
+		temp[i] = np.sum(subs.g['mass'][use] * subs.g['temp'][use]) / np.sum(subs.g['mass'][use])
+	return kb.in_units('keV K**-1') * temp
+
+
+class TemperatureProfiles(HaloProperties):
+	@classmethod
+	def name(self):
+		return "Tew_profile", "Tmw_profile"
+
+	def plot_x0(cls):
+		return 0.05
+
+	@classmethod
+	def plot_xdelta(cls):
+		return 0.1
+
+	@classmethod
+	def plot_xlabel(cls):
+		return "R/kpc"
+
+	@classmethod
+	def plot_ylog(cls):
+		return False
+
+	@classmethod
+	def plot_xlog(cls):
+		return False
+
+	@staticmethod
+	def plot_ylabel():
+		return "T$_{ew}$ keV", "T$_{mw}$ keV"
+
+	def requires_property(self):
+		return ["SSC", "Rvir"]
+
+	def calculate(self, halo, existing_properties):
+		halo['pos'] -= existing_properties['SSC']
+		halo.wrap()
+		delta = self.plot_xdelta()
+		nbins = int(existing_properties['Rvir']/ delta)
+		maxrad = delta * (nbins + 1)
+		ps = pynbody.analysis.profile.Profile(halo.s, type='lin', ndim=2, min=0, max=maxrad, nbins=nbins)
+		Tew = ps['Tew']
+		Tmw = ps['Tmw']
+		return Tew, Tmw
