f71ec5a420e05827a872d363f18a4ceb00812d3a
mtremmel <m.tremmel6@gmail.com>
2017-11-12 17:03:22 -0500

    correctly use temp cut where it is defined

--- michaels_properties/profiles.py
+++ michaels_properties/profiles.py
@@ -23,11 +23,11 @@ def Tew(self):
 	temp = np.zeros(self.nbins)
 	for i in range(self.nbins):
 		subs = self.sim[self.binind[i]]
-		use = np.where(subs.g['temp'] > temp_cut)[0]
+		#use = np.where(subs.g['temp'] > temp_cut)[0]
 		mu = 0.58
-		tc = tcool(subs.g['rho'][use].in_units('g cm**-3'), subs.g['temp'][use], mu)
-		em = emissivity(subs.g['rho'][use].in_units('g cm**-3'), subs.g['temp'][use], mu, tc)
-		temp[i] = np.sum(em * subs.g['temp'][use]) / np.sum(em)
+		tc = tcool(subs.g['rho'].in_units('g cm**-3'), subs.g['temp'], mu)
+		em = emissivity(subs.g['rho'].in_units('g cm**-3'), subs.g['temp'], mu, tc)
+		temp[i] = np.sum(em * subs.g['temp']) / np.sum(em)
 
 	return kb.in_units('keV K**-1') * temp
 
@@ -37,8 +37,8 @@ def Tmw(self):
 	temp = np.zeros(self.nbins)
 	for i in range(self.nbins):
 		subs = self.sim[self.binind[i]]
-		use = np.where(subs.g['temp'] > temp_cut)[0]
-		temp[i] = np.sum(subs.g['mass'][use] * subs.g['temp'][use]) / np.sum(subs.g['mass'][use])
+		#use = np.where(subs.g['temp'] > temp_cut)[0]
+		temp[i] = np.sum(subs.g['mass'] * subs.g['temp']) / np.sum(subs.g['mass'])
 	return kb.in_units('keV K**-1') * temp
 
 @pynbody.analysis.profile.Profile.profile_property
@@ -46,8 +46,8 @@ def rho_e(self):
 	n_e = np.zeros(self.nbins)
 	for i in range(self.nbins):
 		subs = self.sim[self.binind[i]]
-		use = np.where(subs.g['temp'] > temp_cut)[0]
-		n_e[i] = np.sum(subs.g['ne'][use] * subs.g['mass'][use].in_units('m_p'))/self._binsize.in_units('cm**'+str(int(self.ndim)))[i]
+		#use = np.where(subs.g['temp'] > temp_cut)[0]
+		n_e[i] = np.sum(subs.g['ne'] * subs.g['mass'].in_units('m_p'))/self._binsize.in_units('cm**'+str(int(self.ndim)))[i]
 	return n_e
 
 
