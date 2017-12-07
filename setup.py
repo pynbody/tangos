e5f15d862d56e3d451d1b8aed81678f3fb571eb5
Andrew Pontzen <a.pontzen@ucl.ac.uk>
2017-12-07 11:48:00 +0000

    Add packages for web interface

--- setup.py
+++ setup.py
@@ -13,6 +13,12 @@ install_requires = [
     'PasteDeploy >= 1.5.0', # py3 compat
     'plaster',
     'plaster_pastedeploy',
+    'pyramid',
+    'waitress',
+    'pyramid_debugtoolbar',
+    'pyramid_jinja2',
+    'pyramid_tm','pyramid_retry',
+    'zope.sqlalchemy','pillow',
     'hupper',
     'six',
     ]
@@ -57,7 +63,7 @@ setup(name='tangos',
       },
       include_package_data=True,
       zip_safe=False,
-      python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
+      python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
       install_requires=install_requires,
       tests_require=tests_require,
       test_suite="nose.collector"
