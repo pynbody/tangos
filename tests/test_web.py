211ca2e2cbd191bdbe076b147966a104000e0663
Andrew Pontzen <a.pontzen@ucl.ac.uk>
2017-12-10 19:10:02 +0000

    Fix StringIO vs BytesIO issue in web+py3

--- tests/test_web.py
+++ tests/test_web.py
@@ -1,8 +1,19 @@
 # TODO: expand these tests!
-import tangos.web
+import tangos, tangos.web
+from tangos import testing
 from webtest import TestApp
 
+
 def setup():
+    testing.init_blank_db_for_testing()
+
+    creator = tangos.testing.TestSimulationGenerator()
+
+    halo_offset = 0
+    for ts in range(1,4):
+        creator.add_timestep()
+        creator.add_objects_to_timestep(4)
+
     global app
     app = TestApp(tangos.web.main({}))
 
@@ -11,3 +22,23 @@ def test_root_page():
     response = app.get("/")
     assert response.status_int==200
     assert "table" in response
+
+def test_simulation_page():
+    response = app.get("/sim")
+    assert response.status_int==200
+    assert "table" in response
+
+def test_timestep_page():
+    response = app.get("/sim/ts1")
+    assert response.status_int==200
+    assert "table" in response
+
+def test_halo_page():
+    response = app.get("/sim/ts1/halo_1")
+    assert response.status_int == 200
+    assert "table" in response
+
+def test_plot():
+    response = app.get("/sim/ts1/halo_1/t()/vs/z().png")
+    assert response.status_int == 200
+    assert response.content_type == 'image/png'
