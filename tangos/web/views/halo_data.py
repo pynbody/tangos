ce60e172edb0c6d3d9a0c1038355ea3012da32c8
Andrew Pontzen <a.pontzen@ucl.ac.uk>
2017-12-11 10:35:00 +0000

    Fix broken pickle load in python 3. Add test for image plotting.

--- tangos/web/views/halo_data.py
+++ tangos/web/views/halo_data.py
@@ -203,7 +203,11 @@ def image_plot(request, val, property_info):
     with _matplotlib_lock:
         start(request)
 
-        width = property_info.plot_extent()
+        if property_info:
+            width = property_info.plot_extent()
+        else:
+            width = 1.0
+
         if log and len(val.shape)==2:
             data = np.log10(val)
             data[data!=data]=data[data==data].min()
@@ -217,12 +221,13 @@ def image_plot(request, val, property_info):
         else :
             p.imshow(data)
 
-        p.xlabel(property_info.plot_xlabel())
-        p.ylabel(property_info.plot_ylabel())
+        if property_info:
+            p.xlabel(property_info.plot_xlabel())
+            p.ylabel(property_info.plot_ylabel())
 
         if len(val.shape) is 2 :
             cb = p.colorbar()
-            if property_info.plot_clabel() :
+            if property_info and property_info.plot_clabel() :
                 cb.set_label(property_info.plot_clabel())
 
         return finish(request)
