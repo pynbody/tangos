27c58aa7d2eeb3535b2677bc4ad949f9eeb7e9e6
Andrew Pontzen <a.pontzen@ucl.ac.uk>
2018-08-26 13:54:40 +0100

    Add some useful additional basic halo properties

--- tangos/properties/pynbody/eagle.py
+++ tangos/properties/pynbody/eagle.py
@@ -3,37 +3,10 @@ from ...input_handlers import eagle
 from ...log import logger
 import numpy as np
 
-class EagleParentFinder(PynbodyPropertyCalculation):
+class EagleBH(PynbodyPropertyCalculation):
     works_with_handler = eagle.EagleLikeInputHandler
 
-    names = "parent", "original_subgroup_id"
-
-    def preloop(self, sim, db_timestep):
-        # generate map from original group number to tangos DB object
-        self._groups_objs = {}
-        for o in db_timestep.groups:
-            self._groups_objs[o.finder_id] = o
+    names = "BH_mass", "BH_mdot"
 
     def calculate(self, particle_data, halo_entry):
-        subhalo = particle_data['SubGroupNumber'][0]
-        parent = particle_data['GroupNumber'][0]
-        parent_obj = self._groups_objs[parent]
-        return parent_obj, subhalo
-
-class EagleChildFinder(PynbodyPropertyCalculation):
-    works_with_handler = eagle.EagleLikeInputHandler
-    requires_particle_data = False
-    names = "child"
-
-
-    def preloop(self, sim, db_timestep):
-        # generate map from original halo number to tangos DB object
-        self._halos_objs = {}
-        for o in db_timestep.halos:
-            self._halos_objs[o.finder_id] = o
-
-    def calculate(self, particle_data, halo_entry):
-        subhalo = particle_data['SubGroupNumber'][0]
-        parent = particle_data['GroupNumber'][0]
-        parent_obj = self._groups_objs[parent]
-        return parent_obj, subhalo
+        return particle_data.bh['BH_Mass'].sum(), particle_data.bh['BH_Mdot'].sum()
--- tangos/properties/pynbody/mass.py
+++ tangos/properties/pynbody/mass.py
@@ -6,3 +6,9 @@ class Masses(PynbodyPropertyCalculation):
     def calculate(self, halo, existing_properties):
         return halo['mass'].sum()
 
+
+class MassBreakdown(PynbodyPropertyCalculation):
+    names = "finder_dm_mass", "finder_star_mass", "finder_gas_mass"
+    
+    def calculate(self, halo, existing_properties):
+        return halo.dm['mass'].sum(), halo.star['mass'].sum(), halo.gas['mass'].sum()
