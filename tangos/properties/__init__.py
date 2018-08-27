9561f3065d07bf4ba95e3599e08f01b07f2b83be
Andrew Pontzen <a.pontzen@ucl.ac.uk>
2018-08-27 13:04:08 +0100

    Add lru_cache fallback implementation for python 2 compatibility

--- tangos/properties/__init__.py
+++ tangos/properties/__init__.py
@@ -7,6 +7,7 @@ import importlib
 import warnings
 import functools
 from .. import input_handlers
+from .. import util
 
 
 class PropertyCalculationMetaClass(type):
@@ -362,8 +363,7 @@ def all_properties(with_particle_data=True):
 
     return pr
 
-
-@functools.lru_cache()
+@util.lru_cache()
 def providing_class(property_name, handler_class=None, silent_fail=False):
     """Return property calculator class for given property name when files will be loaded by specified handler.
 
@@ -385,7 +385,7 @@ def providing_class(property_name, handler_class=None, silent_fail=False):
     else:
         raise NameError("No providing class for property " + property_name)
 
-@functools.lru_cache()
+@util.lru_cache()
 def all_providing_classes(property_name):
     """Return all the calculator classes for the given property name (possibly multiple, for different handlers)"""
     classes = all_property_classes()
