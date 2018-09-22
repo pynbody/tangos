aceb637a0bbb50317c500d789aa7bdbfc8e80da3
Andrew Pontzen <a.pontzen@ucl.ac.uk>
2018-09-22 16:09:11 +0100

    Bump version to 1.0.10

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='tangos',
-      version='1.0.9',
+      version='1.0.10',
       description='TANGOS, the agile numerical galaxy organisation system',
       classifiers=[
           "Development Status :: 4 - Beta",
