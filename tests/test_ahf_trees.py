b83c03be9a914840188172cfa95e5b708dde6c79
Tobias Buck <buck@mpia.de>
2019-03-15 14:15:53 +0100

    test for ahf merger tree importer added

--- tests/test_ahf_trees.py
+++ tests/test_ahf_trees.py
@@ -11,29 +11,29 @@ def setup():
     testing.init_blank_db_for_testing()
     tangos.config.base = os.path.join(os.path.dirname(__file__), "test_simulations")
 
-    manager = tools.add_simulation.SimulationAdderUpdater(input_handlers.pynbody.ChangaInputHandler("test_tipsy"))
+    manager = tools.add_simulation.SimulationAdderUpdater(input_handlers.pynbody.ChangaInputHandler("test_ahf_merger_tree"))
     with log.LogCapturer():
         manager.scan_simulation_and_add_all_descendants()
 
 def test_property_import():
     importer = tools.property_importer.PropertyImporter()
-    importer.parse_command_line("X Y Z Mvir --for test_tipsy".split())
+    importer.parse_command_line("X Y Z Mvir --for test_ahf_merger_tree".split())
     with log.LogCapturer():
         parallel_tasks.use('multiprocessing')
         parallel_tasks.launch(importer.run_calculation_loop,2)
 
-    Mvir_test, = tangos.get_timestep("test_tipsy/tiny_000832").calculate_all("Mvir")
+    Mvir_test, = tangos.get_timestep("test_ahf_merger_tree/tiny_000832").calculate_all("Mvir")
     npt.assert_allclose(Mvir_test, [2.34068e+11,   4.94677e+10,   4.58779e+10,   4.24798e+10,   2.31297e+10,   2.1545e+10,   1.85704e+10,   1.62538e+10,   1.28763e+10])
 
 def test_consistent_tree_import():
     importer = tools.ahf_merger_tree_importer.AHFTreesImporter()
-    importer.parse_command_line("--for test_tipsy".split())
+    importer.parse_command_line("--for test_ahf_merger_tree".split())
     with log.LogCapturer():
         importer.run_calculation_loop()
-    assert (tangos.get_timestep("test_tipsy/tiny_000832").calculate_all("finder_id", object_typetag='halo')[0]==[0, 1, 2, 3, 4, 5, 6, 7, 8]).all()
-    testing.assert_halolists_equal(tangos.get_timestep("test_tipsy/tiny.000832").calculate_all("earlier(1)", object_typetag='halo')[0],
-                                   ["test_tipsy/tiny.000640/halo_2",
-                                    "test_tipsy/tiny.000640/halo_3"])
+    assert (tangos.get_timestep("test_ahf_merger_tree/tiny_000832").calculate_all("finder_id", object_typetag='halo')[0]==[0, 1, 2, 3, 4, 5, 6, 7, 8]).all()
+    testing.assert_halolists_equal(tangos.get_timestep("test_ahf_merger_tree/tiny.000832").calculate_all("earlier(1)", object_typetag='halo')[0],
+                                   ["test_ahf_merger_tree/tiny.000640/halo_2",
+                                    "test_ahf_merger_tree/tiny.000640/halo_3"])
 
     assert tangos.get_halo("%/%640/halo_7").next == tangos.get_halo("%/%832/halo_1")
 
