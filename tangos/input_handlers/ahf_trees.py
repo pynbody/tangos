1d2347414483ebdb32554d2b61c3389550a9ec30
Tobias Buck <buck@mpia.de>
2019-03-20 23:34:35 +0100

    changes made to handle ahf mtree files with different format

--- tangos/input_handlers/ahf_trees.py
+++ tangos/input_handlers/ahf_trees.py
@@ -30,6 +30,24 @@ class AHFTree(object):
 
     def _load_raw_links(self):
         """
+        There is at least two different file formats for the AHF mtree files. One includes information about the particles shared across haloes,
+        the other not and simply orders the progenitors by a merit function.
+        This little function is to decide which file format we are dealing with.
+        """
+
+        try:
+            self._load_mtree_file_standard()
+        except:
+            logger.info("Could not load AHF mtree file in standard format. Trying the non-standard form.")
+            try:
+                self._load_mtree_file_cropped()
+            except:
+                logger.info("Could not load AHF mtree file in non-standard format either. Make sure mtree files exist.")
+                raise IOError("Could not load merger tree files")
+
+
+    def _load_mtree_file_standard(self):
+        """
         read in the AHF mtree file containing the indices of halos and its progenitors as well as information about the shared particles.
         We establish symmetric links since for AHF any progenitor can have several descendants cause of mass transfer.
         """
@@ -48,6 +66,35 @@ class AHFTree(object):
                     results['f_share'] = np.append(results['f_share'], float(data[idx][0] * data[idx][0]) / (data[i][1] * data[idx][2]) )
             i += data[i][2] + 1
         
+        self.links = results
+
+    def _load_mtree_file_cropped(self):
+        """
+        read in the AHF mtree file containing only the indices of halos and its progenitors and assume progenitors are ordered in descending weight.
+        """
+        filename = self._path
+        results = {'id_this':np.asarray([],dtype=np.int64), 'id_desc':np.asarray([],dtype=np.int64), 'Merit':np.asarray([],dtype=np.float64)} #'Mvir':np.asarray([],dtype=np.float64), 
+
+        f = open(filename)
+        lines = f.readlines()
+        nhalos = int(lines[0])
+        skip = 1 #skip lines already read
+        i=0
+        while i < nhalos:
+            i += 1
+            _tmp = np.fromstring(lines[skip], dtype=np.dtype(int,int), sep=' ')
+            _id = int(_tmp[0])
+            nprogen = int(_tmp[1])
+            skip += 1 # increment the skip of lines for the line read above
+            if nprogen > 0:
+                for n in range(nprogen):
+                    _this_id = int(lines[skip+n][4:]) # rip off the timestep which is encoded as the first 3 digits
+                    if _this_id in self._fid: # check if the halo exists in the database, this is needed if db was created with a minimum particle number per halo which does not agree with AHF definition 
+                        results['id_desc'] = np.append(results['id_desc'],np.asarray([int(str(_id)[4:])],dtype=np.int64))
+                        results['id_this'] = np.append(results['id_this'],np.asarray([_this_id],dtype=np.int64))
+                        #results['Mvir'] = np.append(results['Mvir'], np.asarray([self._Mvir[self._fid == _this_id]]))
+                        results['Merit'] = np.append(results['Merit'], np.asarray([(nprogen-n)/nprogen],dtype=np.float64))
+            skip += nprogen   # increment line skip by already read lines   
         self.links = results
 
     def _load_major_progenitor_branch(self):
