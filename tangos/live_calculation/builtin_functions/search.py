1731c02c88e05e152a56be7077a0dd924dd3a8b6
Andrew Pontzen <a.pontzen@ucl.ac.uk>
2020-09-18 21:02:38 +0100

    Improve performance of searches (latest, earliest, find_progenitor etc)
    While improving this performance, also identified a bug in find_progenitor, which was
    unable to deal with missing data. Fixed and added tests.

--- tangos/live_calculation/builtin_functions/search.py
+++ tangos/live_calculation/builtin_functions/search.py
@@ -13,27 +13,33 @@ def _find_progenitor_or_descendant(source_halos, property_proxy, property_criter
     if len(source_halos) == 0:
         return []
 
-    all_major_progs = strategy(source_halos)
+    all_major_progs_strategy = strategy(source_halos)
 
-    sources = all_major_progs.sources()
+    sources_for_each_value = all_major_progs_strategy.sources()
 
     property_and_obj = MultiCalculation(ReturnInputHalos(), property_proxy.name)
 
-    with all_major_progs.temp_table() as tt:
+    with all_major_progs_strategy.temp_table() as tt:
         # the query has to explicitly include the source_id, otherwise the sqlalchemy dedup
         # process removes duplicates rows (e.g. if there are several null results, only the
         # first will be returned!)
         raw_query = thl.enumerated_halo_query(tt)
         query = property_and_obj.supplement_halo_query(raw_query)
         all_major_progs = [x[1] for x in query.all()]
-        db_objects, values = property_and_obj.values_sanitized(all_major_progs,
-                                                               core.Session.object_session(source_halos[0]))
+        db_objects_for_each_value, values = property_and_obj.values(all_major_progs)
 
-    assert len(values) == len(all_major_progs) == len(sources)
+    assert len(values) == len(all_major_progs) == len(sources_for_each_value)
+
+    # eliminate all None values
+    mask = np.asarray(values)!=None
+    sources_for_each_value = np.asarray(sources_for_each_value)[mask]
+    db_objects_for_each_value = np.asarray(db_objects_for_each_value)[mask]
+    values = np.asarray(values)[mask]
+
     # now re-organize the values so that we have one per source
     values_per_source = {s: [] for s in range(len(source_halos))}
     objs_per_source = {s: [] for s in range(len(source_halos))}
-    for source, value, obj in zip(sources, values, db_objects):
+    for source, value, obj in zip(sources_for_each_value, values, db_objects_for_each_value):
         values_per_source[source].append(value)
         objs_per_source[source].append(obj)
 
@@ -45,13 +51,17 @@ def _find_progenitor_or_descendant(source_halos, property_proxy, property_criter
         if len(vals) == 0:
             results.append(None)
         else:
-            if property_criterion == 'min':
-                index = np.argmin(vals)
-            elif property_criterion == 'max':
-                index = np.argmax(vals)
-            else:
-                assert False  # should not reach this point
-            results.append(objs[index])
+            try:
+                if property_criterion == 'min':
+                    index = np.argmin(vals)
+                elif property_criterion == 'max':
+                    index = np.argmax(vals)
+                else:
+                    assert False  # should not reach this point
+                results.append(objs[index])
+            except ValueError:
+                results.append(None) # argmin/argmax of empty sequence -> no candidate results
+
     return results
 
 @BuiltinFunction.register
@@ -68,3 +78,6 @@ def find_descendant(source_halos, property_proxy, property_criterion):
 
 find_progenitor.set_input_options(0, provide_proxy=True, assert_class = StoredProperty)
 find_progenitor.set_input_options(1, provide_proxy=True, assert_class = FixedInput)
+
+find_descendant.set_input_options(0, provide_proxy=True, assert_class = StoredProperty)
+find_descendant.set_input_options(1, provide_proxy=True, assert_class = FixedInput)
--- tangos/properties/intrinsic.py
+++ tangos/properties/intrinsic.py
@@ -2,9 +2,9 @@ from __future__ import absolute_import
 from . import LivePropertyCalculation
 
 class IntrinsicProperties(LivePropertyCalculation):
-    names = "t","z","a","dbid", "halo_number", "finder_id", "NDM", "NStar", "NGas", "type", "step_path"
+    names = "t","z","a","dbid", "halo_number", "finder_id", "NDM", "NStar", "NGas", "type", "step_path", "path"
 
     def live_calculate(self, halo):
         ts = halo.timestep
         return ts.time_gyr, ts.redshift, 1./(1.+ts.redshift), halo.id, halo.halo_number, halo.finder_id, \
-               halo.NDM, halo.NStar, halo.NGas, halo.object_typecode, str(halo.timestep.path)
+               halo.NDM, halo.NStar, halo.NGas, halo.object_typecode, str(halo.timestep.path), str(halo.path)
