c7547fccdde62899cf8b8ebe8df86ad97ce47fc6
mtremmel <m.tremmel6@gmail.com>
2021-03-01 13:29:59 -0500

    Have timestep cache create halo maps for both catalog_index and finder_id with an additional argument in resolve
    Include additional argument when dealing with timestep cache for Proxy Objects

--- tangos/util/timestep_object_cache.py
+++ tangos/util/timestep_object_cache.py
@@ -13,20 +13,24 @@ class TimestepObjectCache(object):
     def _initialise_cache(self):
         all_objects = self.session.query(core.Halo).filter_by(timestep_id=self._timestep_id).all()
 
-        self._map = {}
+        self._map_catalog = {}
+        self._map_finder = {}
         for obj in all_objects:
             typetag = obj.object_typetag_from_code(obj.object_typecode)
-            if typetag not in self._map:
-                self._map[typetag] = {}
-            self._map[typetag][obj.catalog_index] = obj
+            if typetag not in self._map_catalog:
+                self._map_catalog[typetag] = {}
+            if typetag not in self._map_finder:
+                self._map_finder[typetag] = {}
+            self._map_catalog[typetag][obj.catalog_index] = obj
+            self._map_finder[typetag][obj.finder_id] = obj
 
-    def _ensure_cache(self):
-        if not hasattr(self, "_map"):
+    def _ensure_cache(self, type):
+        if not hasattr(self, "_map_"+type):
             self._initialise_cache()
 
-    def resolve(self, catalog_index, typetag):
-        self._ensure_cache()
+    def resolve(self, identifier, typetag, type='catalog'):
+        self._ensure_cache(type)
         try:
-            return self._map[typetag][catalog_index]
+            return getattr(self, '_map_'+type)[typetag][identifier]
         except KeyError:
             return None
