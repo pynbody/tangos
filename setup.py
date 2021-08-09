f4afbfd6a27d9d38d48625f8ccce133cea237d3f
Andrew Pontzen <a.pontzen@ucl.ac.uk>
2021-08-09 19:20:49 +0100

    Update to version 1.2
    Make setup.py read version number from __init__.py

--- setup.py
+++ setup.py
@@ -33,6 +33,21 @@ tests_require = [
     ]
 
 from setuptools import setup, find_packages
+import codecs
+import os.path
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
 
 from os import path
 this_directory = path.abspath(path.dirname(__file__))
@@ -40,7 +55,7 @@ with open(path.join(this_directory, 'README.md')) as f:
     long_description = f.read()
 
 setup(name='tangos',
-      version='1.1.1',
+      version=get_version("tangos/__init__.py"),
       description='TANGOS, the agile numerical galaxy organisation system',
       classifiers=[
           "Development Status :: 5 - Production/Stable",
