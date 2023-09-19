8487b02cd828a82c016e23cdb4c5b6743f8e974d
Anatole Storck <storckanatole@gmail.com>
2023-09-19 17:02:39 +0200

    support RAMSES data with rockstar halos

--- tangos/util/read_datasets_file.py
+++ tangos/util/read_datasets_file.py
@@ -5,7 +5,6 @@ def read_datasets(basedir,filename):
     if os.path.exists(os.path.join(basedir, "datasets.txt")):
         with open(os.path.join(basedir, "datasets.txt")) as f:
             for l in f:
-                if l.split()[0].endswith(filename):
+                if filename in l.split()[0]:
                     return int(l.split()[1])
-    else:
-        raise AssertionError("Unable to open datasets.txt")
+    raise AssertionError("Unable to open datasets.txt")
