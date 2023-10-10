e68d9c248909cd14e32e7fa76aa43c8363b35427
Andrew Pontzen <a.pontzen@ucl.ac.uk>
2023-10-10 09:37:44 +0100

    Update integration testing to retain tutorial_changa for crosslinking with tutorial_changa_blackholes
    Put this first in the hope that disk space doesn't run out

--- test_tutorial_build/build.sh
+++ test_tutorial_build/build.sh
@@ -115,15 +115,16 @@ detect_mpi
 
 set -e
 
+build_changa
+build_changa_bh
+clearup_files tutorial_changa
+clearup_files tutorial_changa_blackholes
 build_gadget_subfind
 build_gadget_rockstar
 clearup_files tutorial_gadget
+clearup_files tutorial_gadget_rockstar
 build_ramses
 clearup_files tutorial_ramses
-build_changa
-clearup_files tutorial_changa
-build_changa_bh
-clearup_files tutorial_changa_blackholes
 build_gadget4
 clearup_files tutorial_gadget4
 build_enzo_yt
