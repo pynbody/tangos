599b62da92486129821e645fb7d64754660c25b2
Anatole Storck <storckanatole@gmail.com>
2023-11-02 11:52:12 +0100

    removed logging/degugging statements

--- tangos/tools/add_simulation.py
+++ tangos/tools/add_simulation.py
@@ -150,7 +150,8 @@ class SimulationAdderUpdater:
                 halos.append(h)
 
         with pt.ExclusiveLock("db_write_lock"):
-            logger.info("Add %d %ss to timestep %r", len(halos), create_class.__name__, ts)
+            if (create_class.__name__ != "Group"):
+                logger.info("Add %d %ss to timestep %r", len(halos), create_class.__name__, ts)
             self.session.add_all(halos)
             self.session.commit()
 
