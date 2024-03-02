e1fbd7f9c6dfb7353c17a649fc15c2b45f0540be
Andrew Pontzen <andrew.pontzen@gmail.com>
2024-03-02 20:19:58 +0000

    Remove sqlalchemy 1.4 from support

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 install_requires = [
     'setuptools',
     'numpy >= 1.10.0',
-    'sqlalchemy >= 1.4.0', # moving towards sqlalchemy 2.0 eventually
+    'sqlalchemy >= 2.0', 
     'pyparsing >= 2.1.0',
     'WebOb >= 1.7.0rc2', # Response.has_body
     'repoze.lru >= 0.4', # py3 compat
