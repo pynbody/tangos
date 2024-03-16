b3a3fd213f2128856a5c7dbff04e793f06958ec0
Andrew Pontzen <a.pontzen@ucl.ac.uk>
2024-03-16 18:07:48 +0000

    Bump version number and mark as beta until pynbody 2 is out of beta
    Clear up un-needed imports at package level

--- tangos/__init__.py
+++ tangos/__init__.py
@@ -7,28 +7,8 @@ For information on getting started, see README.md.
 
 """
 
-import sqlalchemy
-import sqlalchemy.orm.session
-from sqlalchemy import (
-    Boolean,
-    Column,
-    DateTime,
-    Float,
-    ForeignKey,
-    Index,
-    Integer,
-    String,
-    and_,
-    create_engine,
-    or_,
-    orm,
-)
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import backref, clear_mappers, deferred, relationship, sessionmaker
-from sqlalchemy.orm.session import Session
-
 from . import core, log, properties
 from .core import *
 from .query import *
 
-__version__ = '1.9.1'
+__version__ = '1.9.2-beta'
